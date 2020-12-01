8f81bf6b23f468f82c58ba7a6a12736c53288c83
Caroline <carolinechatelon@hotmail.fr>
2020-12-01 09:46:22 +0100

    create component test

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>Titre 1</h1>
+        <h2>message : {{ msg }}</h2>
+        <p>Seulement le premier mot : {{ onlyFirstWord }}</p>
+        <p>{{ longText(world) }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg:'Hello big world',
+            hello: 'Hello',
+            world:'will world',
+            
+        }
+    },
+   computed:{
+       onlyFirstWord(){
+           return this.msg.split(' ')[0].toUpperCase();
+       },
+       longText(){
+           return function fullText(mot){
+               return this.hello + ' ' + mot
+           }
+       }
+   },
+   created(){
+
+   },
+   mounted(){
+
+   },
+   methods(){
+
+   },
+   
+    
+}
+</script>
+
+<style  scoped>
+
+h1{
+        color:red;
+    }
+
+</style>
